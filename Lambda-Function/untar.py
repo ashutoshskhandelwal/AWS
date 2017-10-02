1a8e5fc57f9505a619e647bd7cccb92757d7a5df
Ashutosh Khandelwal <ashutoshskhandelwal@users.noreply.github.com>
2017-10-02 14:27:26 -0400

    Update untar.py

--- Lambda-Function/untar.py
+++ Lambda-Function/untar.py
@@ -11,17 +11,13 @@ def lambda_handler(event, context):
     new_bucket='uncompressed-data'
     new_key=key[:-4]
     try:
-        if(key[-5:]=='.xlsx'):
-            copy_source = {'Bucket':bucket,'Key':key}
-            s3_resource.meta.client.copy(copy_source, new_bucket, key)
-        else:
-            s3_client.download_file(bucket, key, '/tmp/file')
-            if(tarfile.is_tarfile('/tmp/file')):
-                tar = tarfile.open('/tmp/file', "r:gz")
-                for TarInfo in tar:
-                    tar.extract(TarInfo.name, path='/tmp/extract/')
-                s3_client.upload_file('/tmp/extract/'+TarInfo.name,new_bucket, new_key)
-                tar.close()
+        s3_client.download_file(bucket, key, '/tmp/file')
+        if(tarfile.is_tarfile('/tmp/file')):
+           tar = tarfile.open('/tmp/file', "r:gz")
+           for TarInfo in tar:
+              tar.extract(TarInfo.name, path='/tmp/extract/')
+        s3_client.upload_file('/tmp/extract/'+TarInfo.name,new_bucket, new_key)
+        tar.close()
     except Exception as e:
         print(e)
         raise e
